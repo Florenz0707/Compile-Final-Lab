932258792d2555be0ca854441c85570d6a9c002b
AII12754 <aii1275438796@gmail.com>
2025-12-05 22:43:44 +0800

    [style] Fix some error.

--- compiler_ir/main.cpp
+++ compiler_ir/main.cpp
@@ -127,7 +127,7 @@ void runAllTestcases(const std::string& testDir) {
 
         {
             std::ofstream tokFile(stem + ".tok");
-            tokFile << "File: " << testCase << std::endl;
+            // tokFile << "File: " << testCase << std::endl;
             for (const auto& token : tokens) {
                 if (token.type != TokenType::END_OF_FILE) {
                     tokFile << token.toString() << std::endl;
